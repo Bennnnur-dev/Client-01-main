bf07ca139e085ba725a042d45ec2ba19bdb5495d
Bennnnur <benjamin.axcell@outlook.com>
2025-03-08 10:29:21 +0100

    service tab

--- scripts/header-animation.js
+++ scripts/header-animation.js
@@ -1,6 +1,8 @@
 const header = document.getElementById('header-nav')
 const topMarker = document.querySelector('.hero-section-title-bg')
 const background = document.getElementById('header-background')
+const serviceButton = document.getElementById('service-tab')
+const scrollTab = document.getElementById('scroll-tab')
 
 document.addEventListener('DOMContentLoaded', checkDistance)
 document.addEventListener('scroll', checkDistance)
@@ -12,3 +14,8 @@ function checkDistance(){
     if (headerY - markerY > -10) return background.style.transform = 'translateY(0vw)'
     background.style.transform = 'translateY(-200%)'
 }
+
+serviceButton.addEventListener('hover', () => {
+    scrollTab.style.display = 'flex'
+    console.log('z')
+})
