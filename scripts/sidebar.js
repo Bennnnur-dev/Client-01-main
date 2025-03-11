e76cf130cd1ccb85eab60eab18c73d58082c7e5f
Bennnnur <benjamin.axcell@outlook.com>
2025-03-11 21:34:40 +0100

    upgraded sidebar

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -3,9 +3,9 @@ const sidebar = document.getElementById("sidebar")
 const sidebarButton = document.getElementById("sidebar-button")
 
 extButton.addEventListener("click", () => {
-    sidebar.style.display = "none"
+    sidebar.style.transform = "translateX(200vw)"
 })
 
 sidebarButton.addEventListener("click", () => {
-    sidebar.style.display = "flex"
+    sidebar.style.transform = "translateX(0)"
 })
